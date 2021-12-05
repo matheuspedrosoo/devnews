45a34a9864b51e4edaf982e01f4b6e774417101e
Matheus Pedroso <matheuspedroso404@gmail.com>
2021-12-05 13:55:46 -0300

    create component Header and ActivLink

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import { ActiveLink } from '../../components/ActiveLink';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/logo.svg" alt="DevNews" />
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
