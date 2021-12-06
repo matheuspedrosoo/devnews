51c06398240b3baeea685e229dea4bb6b0470054
Matheus Pedroso <matheuspedroso404@gmail.com>
2021-12-05 21:26:08 -0300

    created and stylized page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,28 @@
-import { GetServerSideProps } from 'next';
-import { useEffect, useState } from 'react';
+/* eslint-disable @next/next/no-img-element */
 import SEO from '../components/SEO';
 
-interface Post {
-  id: string;
-  title: string;
-}
+import styles from '../styles/home.module.scss';
 
-interface HomeProps {
-  posts: Post[];
-}
-
-export default function Home({ posts }: HomeProps) {
-  // const [posts, setPosts] = useState<Post[]>([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/posts').then(response => {
-  //     response.json().then(data => {
-  //       setPosts(data);
-  //     });
-  //   });
-  // }, []);
-
+export default function Home() {
   return (
-    <div>
-      <SEO title="Home" />
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SEO title="Dev News!" excludeTitleSuffix />
+
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Olá Dev</span>
+          <h1>
+            Bem-vindo e bem-vinda <br />
+            ao <span>Dev</span>News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br />
+            <span>relevantes para o seu aprendizado.</span>
+          </p>
+        </section>
+
+        <img src="/home.svg" alt="Home image" />
+      </main>
+    </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
