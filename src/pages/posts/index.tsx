08c608729e6a289f39971662a58d23f614edef7e
unknown <matheuspedroso404@gmail.com>
2021-12-06 14:18:20 -0300

    created page slug

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -26,7 +26,7 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <Link key={post.slug} href="#">
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
               <a href="">
                 <time>{post.updateAt}</time>
                 <strong>{post.title}</strong>
